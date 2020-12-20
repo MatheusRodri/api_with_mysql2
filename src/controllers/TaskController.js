29b8d781e793eaa0eff488f66a7850d85414c246
Matheus Rodrigues De Jesus <mathclash25@gmail.com>
2020-12-20 20:25:03 -0300

    api 100% funcionando

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -31,6 +31,27 @@ class TaskController{
                 console.log(error)
             })
     }
+    atualizarMorador(request,response){
+        const id_morador = request.params
+        const {nm_morador,apto_morador,bl_morador,email,senha,morador,sindico,porteiro} = request.body
+
+        database.where({id_morador:id_morador.id}).update({nm_morador:nm_morador,apto_morador:apto_morador,bl_morador:bl_morador,email:email,senha:senha,morador:morador,sindico:sindico,porteiro:porteiro}).table("tb_morador").then(data =>{
+            console.log(data)
+            response.json({message:"TUDO DEU CERTO"})
+        }).catch(error=>{
+            response.json(error)
+        })
+    }
+
+    removerMorador(request,response){
+        const id = request.params
+
+            database.where({id_morador:id.id}).del().table("tb_morador").then(data =>{
+                    response.json({message:"MORADOR DELETADO"})
+            }).catch(error=>{
+                response.json(error)
+            })
+    }   
 }
 
 module.exports = new TaskController()
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,5 +6,7 @@ const TaskController = require("../controllers/TaskController")
 router.post("/novoMorador",TaskController.novaMorador)
 router.get("/morador",TaskController.listaMorador)
 router.get("/morador/:id_morador",TaskController.listarUmMorador)
+router.put("/atualizar/morador/:id",TaskController.atualizarMorador)
+router.delete("/deletar/morador/:id",TaskController.removerMorador)
 
 module.exports = router
