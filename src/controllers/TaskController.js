1ab72163ad965ba630abd8398478b6ab52d5e9bc
Matheus Rodrigues De Jesus <mathclash25@gmail.com>
2020-12-20 21:12:29 -0300

    Update TaskController.js

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -4,10 +4,7 @@ class TaskController{
     novaMorador(request,response){
         const{nm_morador,apto_morador,bl_morador,email,senha,morador,sindico,porteiro} = request.body
 
-        console.log(nm_morador,apto_morador,bl_morador,email,senha,morador,sindico,porteiro)
-
         database.insert({nm_morador,apto_morador,bl_morador,email,senha,morador,sindico,porteiro}).table("tb_morador").then(data=>{
-            console.log(data)
             response.json({message:"Certo"})
         }).catch(error=>{
             console.log(error)
@@ -15,7 +12,6 @@ class TaskController{
     }
     listaMorador(request,response){
         database.select("*").table("tb_morador").then(morador =>{
-            console.log(morador)
             response.json(morador)
     }).catch(error =>{
         console.log(error)
@@ -23,7 +19,6 @@ class TaskController{
     }
     listarUmMorador(request,response){
         const id_morador = request.params
-        console.log(id_morador.id_morador)
 
         database.select('*').table("tb_morador").where({id_morador:id_morador.id_morador}).then(morador=>{
             response.json(morador)
@@ -36,7 +31,6 @@ class TaskController{
         const {nm_morador,apto_morador,bl_morador,email,senha,morador,sindico,porteiro} = request.body
 
         database.where({id_morador:id_morador.id}).update({nm_morador:nm_morador,apto_morador:apto_morador,bl_morador:bl_morador,email:email,senha:senha,morador:morador,sindico:sindico,porteiro:porteiro}).table("tb_morador").then(data =>{
-            console.log(data)
             response.json({message:"TUDO DEU CERTO"})
         }).catch(error=>{
             response.json(error)
@@ -45,7 +39,6 @@ class TaskController{
 
     removerMorador(request,response){
         const id = request.params
-
             database.where({id_morador:id.id}).del().table("tb_morador").then(data =>{
                     response.json({message:"MORADOR DELETADO"})
             }).catch(error=>{
